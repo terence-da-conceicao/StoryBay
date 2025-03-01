21899aa9b2ff64ef2d94ee00f5e9ab32627a1f05
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2025-03-01 06:21:02 +0100

    fonction de retrait de livre. Ne fonctionne qu'avec le premier, corriger

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// const booksList = [
-//     {title: "Henri Pottier 1", price: 10},
-//     {title: "Henri Pottier 2", price: 10},
-//     {title: "Henri Pottier 3", price: 10},
-//     {title: "Henri Pottier 4", price: 10},
-//     {title: "Henri Pottier 5", price: 10}
-// ]
+let booksList 
+let offers
+let isBookAvailable = false;
+let chosenBook;
+let cart = [];
+let cartText = document.getElementById("cartText");
+const viewCart = document.getElementById("cart");
 
 
 
@@ -16,101 +16,93 @@ async function fetchData(data) {
         if (!response.ok) {
             throw new Error(`Response status : ${response.status}`);
         }
-        let jsonData = await response.json()
+        let jsonData = await response.json();
         return jsonData;
     } catch(error) {
         console.error(error.message);
     }
-
-   
 }
-let booksList 
-let offers
-async function useData() {
+
+
+async function getData() {
     offers = await fetchData('offers.json');
     booksList = await fetchData('books.json');
     createDatalist();
 }
+getData()
 
-useData()
-
-
-
-// async function fetchOffers(data) {
-//     fetch(data)
-//         .then(response => {
-//             if (!response.ok) {
-//                 throw new Error(`HTTP error! Status: ${response.status}`);
-//             }
-//             return response.json();
-//         })
-//         .then(data => console.log(data))
-//         .catch(error => console.error('Error fetching JSON : ', 
-//     error));
-// }
-// let offers = fetchOffers('offers.json')
-// console.log(offers)
-
-// getData()
-
-
-let isBookAvailable = false;
-let chosenBook;
-let cart = [];
 
 async function createDatalist() {
-    
     const datalist = document.getElementById("books");
-    for (const book in booksList) {
-        // console.log(booksList[book].title)
-    // booksList.forEach(book => {
+        for (book in booksList) {
         const option = document.createElement("option");
         option.value = booksList[book].title;
         datalist.appendChild(option);
-    }
+    };
 }
 
 
 
 document.querySelector("form").addEventListener("submit", getBook);
 
-
 function getBook(event) {
     event.preventDefault()
     const inputValue = document.getElementById("inputValue").value.toLowerCase();
-    console.log(inputValue);
 
     for (let i = 0; i < booksList.length; i++) {
         let loweredBook = booksList[i].title.toLowerCase()
-        console.log(loweredBook)
         if (inputValue === loweredBook) {
             isBookAvailable = true;
             chosenBook = booksList[i];
-            console.log("isBookAvailable : ", isBookAvailable);
             createBookCard(chosenBook);
             return booksList[i].title
         }
     }
     if (!isBookAvailable) {
-        document.getElementById("bookChoice").innerHTML = `Ce livre n'est pas disponible.`;
-        console.log("isBookAvailable : ", isBookAvailable);
+        document.getElementById("bookChoice").innerHTML = "Ce livre n'est pas disponible.";
     }
 }
 
 
 function createBookCard(book){
-    document.getElementById("bookChoice").innerHTML = `<h3>${book.title}</h1>`;
+    document.getElementById("bookChoice").innerHTML = `<h3>${book.title}</h3>`;
     document.getElementById("buyBook").style.display = "block";
 }
 
-const displayCart = document.getElementById("cart")
-
-function addToCart() {
+document.getElementById("cartControls").addEventListener("click", event => {
+    event.preventDefault();
     cart.push(chosenBook);
-    console.log("cart:",cart)
-    console.log(`${chosenBook.title}, ${chosenBook.price}$`)
+    console.log("cart : ",cart)
+    displayCart()
+})
+
+function displayCart() {
+    if (viewCart.style.display === "none") {
+        viewCart.style.display = "block";
+    }
+    displayItem()
+}
+
+function displayItem() {
     const li = document.createElement("li");
+    let indexBook = cart.length-1;
+    li.dataset.index = indexBook;
     li.innerHTML = `${chosenBook.title}, ${chosenBook.price}`;
-    displayCart.appendChild(li);
+    viewCart.appendChild(li);
+
+
+    const remove = document.createElement("button");
+    remove.innerText = 'Retirer';
+    let removeButton = document.getElementById("removeId")
+    removeButton.addEventListener("click", deleteItem);
+    li.appendChild(remove)
+}
+
+
+function deleteItem(event) {
+    const li = event.target.closest("li");
+    const indexBook = parseInt(li.dataset.index);
+    cart.splice(indexBook ,1)
+    li.remove()
 }
 
