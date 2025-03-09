fa6b2fc6f1afa026de29884d7e7e288ab8c612c7
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2025-03-09 23:17:36 +0100

    abandon de la fonctionnalité de navigation sur deux pages. Il aurait fallu une meilleure conception avant.

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ import { addToCart, setBookState } from "./src/controller/bookController.js"
 import { displayBookCard } from "./src/view/bookView.js"
 import { displayCart, displayFinalTotal } from "./src/view/cartView.js"
 import { getData } from "./src/controller/dataController.js"
-import { useRoute } from "./src/router.js"
-
 
 const booksList = await getData('src/model/booksList.json')  //récupération des models booksList et offers.//idéalement il faudrait les passer avec des arguemnts, non?
 const offers = await getData("src/model/offers.json");
@@ -26,12 +24,6 @@ let displayTotalEl = document.getElementById("displayTotalEl");
 createDatalist(booksList) //affichage du model booksList sous la forme d'une datalist
 
 //soumettre le livre choisi par le user
-// form.addEventListener("submit", (event) => {
-//     event.preventDefault(), 
-//     updateBook(selectedBook, booksList, isBookAvailable)
-//     displayBookCard(isBookAvailable, selectedBook, addButton) // on affiche la bookCard (titre + bouton d'ajout)
-// });
-
 form.addEventListener("submit", (event) => {
     event.preventDefault(), 
     updateBook()
