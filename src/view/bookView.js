65ca55276058a26d196f0876afcf2554b68d0dd7
terence-da-conceicao <158314737+terence-da-conceicao@users.noreply.github.com>
2025-03-13 15:29:38 +0100

    réaffichage correct de la bookCard

--- src/view/bookView.js
+++ src/view/bookView.js
@@ -1,30 +1,33 @@
 export function displayBookCard(state, book, button){
+    const bookCard = document.getElementById("buyBook");
+    const title = document.getElementById("selectedBookTitle");
     const coverImage = document.getElementById("bookCover");
+    const price = document.getElementById("price");
+    const errorCard = document.getElementById("errorCard");
 
-    
-    
-    
     if (state === false) {
-        const errorCard = document.createElement("span");
-        errorCard.innerHTML = "Ce livre n'est pas disponible."
-        errorCard.classList.add("p-4")
-        document.getElementById("selection").appendChild(errorCard)
+        if (!errorCard) {
+            const newErrorCard = document.createElement("span");
+            newErrorCard.id = "errorCard";
+            newErrorCard.innerHTML = "Ce livre n'est pas disponible."
+            newErrorCard.classList.add("p-4")
+            document.getElementById("selection").appendChild(newErrorCard)
+        }
         document.getElementById("buyBook").style.display = "none";
 
-        // document.getElementById("selectedBookTitle").innerHTML = "Ce livre n'est pas disponible.";
-        // coverImage.style.display = "none";
     } else {
+            bookCard.style.display = "block";
+            title.innerHTML = `${book.title}`;
+            price.innerHTML = `${book.price}€`;
+            coverImage.src = book.cover
+            coverImage.alt = `couverture de ${book.title}`;
+            button.style.display = "block";
 
-        document.getElementById("selectedBookTitle").innerHTML = `${book.title}`;
-        document.getElementById("price").innerHTML = `${book.price}€`;
-        
-        coverImage.src = book.cover
-        coverImage.classList.remove('hidden'); // Enlève la classe `hidden` pour l'afficher
-        coverImage.classList.add('block');
-        coverImage.style.display = "block";
-        coverImage.alt = `couverture de ${book.title}`;
-        coverImage.classList.add('book-cover');
 
-        button.style.display = "block";
+            bookCard.classList.remove("hidden");
+
+            if (errorCard) {
+                errorCard.remove(); // Supprime le message d'erreur
+            }
     }
 }
